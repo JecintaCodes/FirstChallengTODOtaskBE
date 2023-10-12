20599f651ccc1af1f16f7c3c9d1c26eb4fb428b1
jecinta2003 <onyemaobijecintaugochi@gmail.com>
2023-10-12 23:35:02 +0100

    first commit

--- config/DataBAse.ts
+++ config/DataBAse.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 
-const todoDB: string = "mongodb://0.0.0.0:27017/BookStoreDataBase"
-// const todoDB: string = "mongodb+srv://Jecinta:<Jecinta>@jecinta.zxc8ay5.mongodb.net/"
+// const todoDB: string = "mongodb://0.0.0.0:27017/BookStoreDataBase"
+const todoDB: string = "mongodb+srv://Jecinta:Jecinta>@jecinta.zxc8ay5.mongodb.net/"
 
 export const ToDoDataBase = async()=>{
     try {
